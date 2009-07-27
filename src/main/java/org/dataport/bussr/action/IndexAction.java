e4f793f44ee30b5ca80afa75bc294cf17df5ef61
tute <matiascas@gmail.com>
2009-07-27 20:33:08 +0200

    Stripes configuration, base action

---
+++ src/main/java/org/dataport/bussr/action/IndexAction.java
@@ -0,0 +1,13 @@
+package org.dataport.bussr.action;
+
+import net.sourceforge.stripes.action.Resolution;
+
+import org.dataport.bussr.action.foundation.BaseAction;
+
+public class IndexAction extends BaseAction {
+
+    public Resolution load() {
+        // TODO
+        return null;
+    }
+}
